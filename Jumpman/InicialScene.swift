b50398f55d16be0aafa25e3ae84955d36f2c93e9
Matheus Henrique de Souza Alves <matheushalves@outlook.com.br>
2016-05-31 13:32:02 -0400

    Montada a tela inicial do jogo.

---
+++ Jumpman/InicialScene.swift
@@ -0,0 +1,27 @@
+//
+//  GameScene.swift
+//  Jumpman
+//
+//  Created by Matheus Alves on 31/05/16.
+//  Copyright (c) 2016 Encosis 2016. All rights reserved.
+//
+
+import SpriteKit
+
+class InicialScene: SKScene {
+    
+    override func didMoveToView(view: SKView) {
+        
+        
+        
+        
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+       /* Called when a touch begins */
+        
+        
+        
+    }
+   
+}
